46a1aec0025fdc71b08db87127fa319f9b07e4f0
elizabethLomb <elizabeth.lombardi@gmail.com>
2025-09-12 17:43:46 +0200

    feat: getaway loading config

---
+++ services/gateway-service/config/development.js
@@ -0,0 +1,12 @@
+const developmentConfig = {
+  server: {
+    port: process.env.PORT || 5000,
+    host: process.env.NODE_SERVER || 'http://127.0.0.1',
+    env: process.env.NODE_ENV || 'development',
+  },
+  services: {
+    'service-auth': 'http://auth-service:5001/api/v1'
+  },
+};
+
+module.exports = developmentConfig;
